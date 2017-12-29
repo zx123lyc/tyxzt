f38f3ece39f39ad64856df66c4e267fcbb596aeb
xzt <2277165212@qq.com>
2017-12-29 15:38:42 +0800

    Add demo.java file

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 ﻿public class Hello{
 	public static void main(String args[]){
-		System.out.println("hello xzt");
-		System.out.println("www.tyxzt.com");
+		System.out.println("www.tyxzt2.com");
 	}
 	
 }
