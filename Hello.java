cc32330a371558c2d34f01ef182c835bde40b062
xzt <2277165212@qq.com>
2017-12-29 15:08:42 +0800

    update Hello.java add two lines

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 ﻿public class Hello{
 	public static void main(String args[]){
-		System.out.println("hello world");
+		System.out.println("hello xzt");
+		System.out.println("www.tyxzt.com");
 	}
 	
 }
